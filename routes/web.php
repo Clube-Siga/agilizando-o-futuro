9064e9ab1d7add3f7b53fa1dfb5435c05db1c67a
JeanFDev <jeanjfra1@gmail.com>
2024-04-17 11:46:21 -0300

    criação de componentes header hero about cta

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,14 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// Route::get('/welcome', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
+Route::get('/welcome', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
 Route::get('/', function () {
     return Inertia::render('Agilizando/Home', [
