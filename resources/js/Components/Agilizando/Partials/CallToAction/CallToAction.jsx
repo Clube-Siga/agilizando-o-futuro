66d95bafcbdee11682f4c8f7b7d2b285b7913b61
Alexandro Messias <108425842+alexandro077@users.noreply.github.com>
2024-04-19 11:06:18 -0300

    add mudanca bottom

--- resources/js/Components/Agilizando/Partials/CallToAction/CallToAction.jsx
+++ resources/js/Components/Agilizando/Partials/CallToAction/CallToAction.jsx
@@ -10,7 +10,7 @@ export default function CallToAction(){
                     <div class="mt-4 md:mt-0">
                         <Title titleContent={'Faça sua doação'}/>
                         <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Flowbite helps you connect with friends and communities of people who share your interests. Connecting with your friends and family as well as discovering new ones is easy with features like Groups.</p>
-                        <Button path={"#"} title={"Comece aqui"} />
+                        <Button buttonClass={"inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"} path={"#about"} title={"Comece aqui"} />
                     </div>
                     {/* <Image altText={"dashboard image"} imgPath={"https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg"} /> */}
                     <Image altText={"dashboard image"} imgPath={"https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg"} />
