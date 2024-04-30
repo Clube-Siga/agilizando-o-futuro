14c6d3e582468d49d85d3b7758a8c2f8920622bb
Alexandro Messias <108425842+alexandro077@users.noreply.github.com>
2024-04-30 18:19:02 -0300

    add mudancas

--- resources/js/Components/Agilizando/Partials/CallToAction/CallToAction.jsx
+++ resources/js/Components/Agilizando/Partials/CallToAction/CallToAction.jsx
@@ -4,8 +4,8 @@ import Title from "../../Components/Title/Title";
 
 export default function CallToAction(){
     return (
-        <>
-<<<<<<<<< Temporary merge branch 1
+        
+
             <section id="cta" className={ctaClass}>
                 <Row rowClass={"gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6"}>
                     <div className="mt-4 md:mt-0">
@@ -17,21 +17,8 @@ export default function CallToAction(){
                     {/* <img className="w-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg" alt="dashboard image" />
                     <img className="w-full hidden dark:block" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="dashboard image" /> */}
                 </Row>
-=========
-            <section class="bg-white dark:bg-gray-900">
-                <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-                    <div class="mt-4 md:mt-0">
-                        <Title titleContent={'Faça sua doação'}/>
-                        <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Flowbite helps you connect with friends and communities of people who share your interests. Connecting with your friends and family as well as discovering new ones is easy with features like Groups.</p>
-                        <Button buttonClass={"inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"} path={"#about"} title={"Comece aqui"} />
-                    </div>
-                    {/* <Image altText={"dashboard image"} imgPath={"https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg"} /> */}
-                    <Image altText={"dashboard image"} imgPath={"https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg"} />
-                    {/* <img class="w-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg" alt="dashboard image" />
-                    <img class="w-full hidden dark:block" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="dashboard image" /> */}
-                </div>
->>>>>>>>> Temporary merge branch 2
+
             </section>
-        </>
+     
     )
 }
