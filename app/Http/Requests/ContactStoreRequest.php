562a03801d675e7286f14c40a5172575309aa2ff
Webert Maximiano <webertcoach@gmail.com>
2024-05-11 19:12:54 -0300

    suporte http

--- app/Http/Requests/ContactStoreRequest.php
+++ app/Http/Requests/ContactStoreRequest.php
@@ -4,6 +4,9 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
+use Illuminate\Support\Facades\Http;
+use Illuminate\Contracts\Validation\Rule;
+
 class ContactStoreRequest extends FormRequest
 {
     /**
