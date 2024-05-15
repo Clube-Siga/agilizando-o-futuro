d981b52b510a66ce40eddc13288778153e88f198
JeanFDev <jeanjfra1@gmail.com>
2024-05-15 15:04:03 -0300

    ajustes visuais

--- resources/js/Components/Agilizando/Components/Headline/Headline.jsx
+++ resources/js/Components/Agilizando/Components/Headline/Headline.jsx
@@ -1,7 +1,7 @@
 export default function Headline({headline}){
     return (
         <>
-            <h1 className="max-w-2xl mb-5 font-body text-4xl font-extrabold tracking-tight leading-none text-secondary md:text-5xl xl:text-6xl dark:text-defaultW">{headline}</h1>
+            <h1 className="max-w-2xl mb-5 font-body text-4xl font-thin tracking-tight leading-none text-secondary md:text-5xl xl:text-6xl dark:text-defaultW">{headline}</h1>
         </>
     )
 }
