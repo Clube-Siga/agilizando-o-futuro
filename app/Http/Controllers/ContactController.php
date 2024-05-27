50ddca335b1aeaa44c8721af7bd39c08f9f88bc5
Webert Maximiano <webertcoach@gmail.com>
2024-05-27 14:07:52 -0300

    logica basica no controlador, classe de servico e teste da classe

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,6 +9,8 @@ use Inertia\Response;
 
 use App\Models\Contact;
 use App\Services\ContactService;
+use App\Services\RecaptchaService;
+
 use Illuminate\Support\Facades\Log; // criar logs
 
 use App\Http\Requests\ContactStoreRequest;
@@ -16,28 +18,57 @@ use App\Http\Requests\ContactStoreRequest;
 class ContactController extends Controller
 {
     private $contactService; // Declarar a variável para o service
+    private $recaptchaService;
 
-    public function __construct(ContactService $contactService) // Injetar a dependência
+    public function __construct(ContactService $contactService, RecaptchaService $recaptchaService) // Injetar a dependência
     {
         $this->contactService = $contactService;
+        $this->recaptchaService = $recaptchaService;
     }
 
-    
+    //falta testar
     public function store (ContactStoreRequest $request)
     { 
         
         try {
 
             $dataValidated = $request->validated();// ok recebendo o token
-           // dd($dataValidated);
-            Log::info("Recebendo: ", json_encode($dataValidated));
+            // dd($dataValidated);            
+            $token = $dataValidated['recaptchaToken']; //acessando token validado
+          
+            $remoteIp = $request->ip(); //pegar o ip do usuario 
 
-            // Create the contact using the service
-            $response = $this->contactService->createContact($request);
+            // Prepara os dados para a requisição à API reCAPTCHA
+            $data = [
+                'secret' => $this->recaptchaService->getSecretKey(), // Sua chave secreta do Google reCAPTCHA
+                'response' => $token, // O token reCAPTCHA do formulário
+                'remoteip' => $remoteIp, // O endereço IP do usuário
+            ];
 
-            Log::info("Executado: ". $response);
+            //Faz uma requisição POST para a API de verificação 
+            $response = $this->recaptchaService->verify($token, $data);
 
-            return to_route('site.index')->with('message', 'Sua mensagem foi enviada com sucesso!');
+            // Verifica se a requisição foi bem-sucedida
+            if ($response->successful()) {
+                               
+                // Decodifica a resposta JSON da API
+                $responseData = $response->json();
+
+                // Verifica se a verificação foi bem-sucedida
+                if (!$responseData['success']) {
+                    return false; // A verificação falhou
+                }
+
+                // Recupera e retorna a pontuação (opcional)
+                if (isset($responseData['score']) && $responseData['score'] > 0.5) { //1.0 muito bom // 0.0 um bot
+                    // cria um contato se for maior que 0.5
+                    $contact = $this->contactService->createContact($request);
+                }
+               
+            } else {
+                // reCAPTCHA verification failed, handle the error
+                return back()->with('error', 'Erro na verificação reCAPTCHA. Tente novamente.');
+            }
             
         } catch (\Exception $e) {
 
---
+++ tests/Feature/RecaptchaServiceTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Http; // metodos prontos
+
+
+// criar uma simulacao pra zombar ou enganar a classe testada simulando o processo real
+use Mockery;
+use Mockery\MockInterface;
+use App\Services\RecaptchaService;
+
+
+class RecaptchaServiceTest extends TestCase
+{
+    //teste para funcionalidade de obter o segredo do recaptcha
+    public function testGetSecretKey()
+    {
+        $recaptchaService = new RecaptchaService();
+
+        // Defina a chave secreta esperada
+        $expectedSecretKey = '6LdgH-gpAAAAABNJEUutDjz1T456Q9ZG2Bntf0vA';
+
+        // Verifique se a chave secreta retornada corresponde à esperada usando o assertEquals retorna verdadeiro se for igual
+        $this->assertEquals($expectedSecretKey, $recaptchaService->getSecretKey());
+    }
+
+    //teste para obter a chave do site do recaptcha
+    public function testGetSitetKey()
+    {
+        $recaptchaService = new RecaptchaService();
+
+        // Defina a chave do site esperada
+        $expectedSiteKey = '6LdgH-gpAAAAADaTUQKIUJd-YZ7-0ZdJT0VjocTG';
+
+        // Verifique se a chave secreta retornada corresponde à esperada usando o assertEquals retorna verdadeiro se for igual
+        $this->assertEquals($expectedSiteKey, $recaptchaService->getSiteKey());
+    }
+
+    // teste para recupera a url da api do recaptcha
+    public function testGetBaseUrl()
+    {
+        $recaptchaService = new RecaptchaService();
+
+        // Defina a url esperada
+        $expectedUrl = 'https://www.google.com/recaptcha/api/siteverify';
+
+        // Verifique se a url retornada corresponde à esperada usando o assertEquals retorna verdadeiro se for igual
+        $this->assertEquals($expectedUrl, $recaptchaService->getBaseUrl());
+    }
+
+    public function testVerify()
+    {
+        // Finge a resposta da API reCAPTCHA
+        Http::fake([
+            'https://www.google.com/recaptcha/api/siteverify' => Http::response([
+                'success' => true,
+                'score' => 0.9,
+                'action' => 'submit',
+                'challenge_ts' => '2024-05-27T12:00:00Z',
+                'hostname' => 'teste.clubesiga.com'
+            ], 200)
+        ]);
+
+        $recaptchaService = new RecaptchaService();
+
+        // Defina o token e os dados de teste vem do post no form
+        $token = '03AFcWeA58d3i07YPEbpfphsETanydY6okp86lwVoOvfY_ZsE4cD2SA--CCR5FGwFzDVl8KJLNyB-5SPTP7a_PFYF9jHS1Ywl3dnT_iD25_JOV5ciXJtp3bb9vL2VDzbpIl-3nOtepzTp6tOV21LQLi8C6nKLuqlDVNiL4aE5kRnLIp_V_bpdY0TV81-4-bugoIngpTAYnm3HFvnNHiTLD2F06iCo24VbQXkXfb6y60Y3vjbRVHeTqmz09j6KzZGaKvyn8c2ZEUgMoHRGtggcH-6lIwuP75vM0jIFfVs7J7P0PfUgEFyJ-z0P6V3JOHKVq-gCl9CbRdUeJJ3c0m9pZLedTi6zxerWHqRho20_La33hCZbVKunLRCj3DMWRhZI0IGjDR2OHZId0EDpTjB3srz6o7iqSHspJ0KJBiXwSuekizBrrILU3OqKlIAVrQIQPmc6So4b2I-1T_2YeE3VYHN_6cf8i5wcSkgvgCoanvtMEjmnyLuCdRwUk7WvxKb1_dMsJc3I-XpyVjF4UwqeD3DS4qTALMrWMrAmKu_BblzXWN401lrQenof0bsG5ynuhyodVNwL85YWDiUhQZ4qxre61AhItXf4W55mYrSNV2d0xYpRWQxenxRDaeVy-AHJw_bvY0T_MCycxhE60WZlhHAMhHX_Bl3H59AGd6UB833AeJOAH6mfd3Oc';
+        
+        
+        $data = [
+            'secret' => $recaptchaService->getSecretKey(),
+            'response' => $token,
+            'remoteip' => '127.0.0.1'
+        ];
+
+        // Chame o método verify
+        $response = $recaptchaService->verify($token, $data);
+
+        // Verifique se a resposta tem sucesso
+        $this->assertTrue($response->json('success'));
+        $this->assertEquals(0.9, $response->json('score'));
+        $this->assertEquals('submit', $response->json('action'));
+        $this->assertEquals('2024-05-27T12:00:00Z', $response->json('challenge_ts'));
+        $this->assertEquals('teste.clubesiga.com', $response->json('hostname'));
+    }
+
+}
