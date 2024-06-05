e002805b158cd64df044675852e1d2f6dbef608d
Webert Maximiano <webertcoach@gmail.com>
2024-06-05 20:45:46 -0300

    ajuste import class

--- app/Mail/ContactReceiveidMail.php
+++ app/Mail/ContactReceiveidMail.php
@@ -7,8 +7,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
-use Illuminate\Queue\SerializesModels;
 use Illuminate\Mail\Mailables\Address;
+use Illuminate\Queue\SerializesModels;
 
 class ContactReceiveidMail extends Mailable
 {
