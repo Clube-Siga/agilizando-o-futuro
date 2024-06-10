b19c4985cefa3678f87cd7a3068c7388cdcaf033
Webert Maximiano <webertcoach@gmail.com>
2024-06-10 17:36:06 -0300

    configurando makdown

--- resources/views/emails/contactReceived.blade.php
+++ resources/views/emails/contactReceived.blade.php
@@ -1,31 +1,23 @@
 @component('mail::message')
 
-    @subject('Aviso: Novo Contato Recebido')
+# Aviso: Novo Contato Recebido
 
-    <p>Prezado SAC,</p>
+Prezado SAC,
 
-    <p>Um novo contato foi recebido através do site Agilizando. Por favor, retorne o contato o mais breve possível.</p>
+Um novo contato foi recebido através do site Agilizando. Por favor, retorne o contato o mais breve possível.
 
-    <h2>Dados do Contato</h2>
+## Dados do Contato
 
-    <table style="width: 100%; border-collapse: collapse;">
-        <tr>
-            <th style="text-align: left; padding: 8px; border: 1px solid #ddd;">Nome</th>
-            <th style="text-align: left; padding: 8px; border: 1px solid #ddd;">Telefone</th>
-            <th style="text-align: left; padding: 8px; border: 1px solid #ddd;">Email</th>
-        </tr>
-        <tr>
-            <td style="text-align: left; padding: 8px; border: 1px solid #ddd;">{{ $emailData['name'] }}</td>
-            <td style="text-align: left; padding: 8px; border: 1px solid #ddd;">{{ $emailData['phone'] }}</td>
-            <td style="text-align: left; padding: 8px; border: 1px solid #ddd;">{{ $emailData['email'] }}</td>
-        </tr>
-    </table>
+| Nome | Telefone | Email |
+| ---- | -------- | ----- |
+| {{ $emailData['name'] }} | {{ $emailData['phone'] }} | {{ $emailData['email'] }} |
 
-    <h2>Mensagem</h2>
-    <p>{{ $emailData['formMessage'] }}</p>
+## Mensagem
 
-    <p>Atenciosamente,</p>
+{{ $emailData['formMessage'] }}
 
-    <p>Equipe Agilizando</p>
+Atenciosamente,
+
+Equipe Agilizando
 
 @endcomponent
