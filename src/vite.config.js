d0acb8d48067879a4355c75c6dbe760848c5f33d
Webert Maximiano <webertcoach@gmail.com>
2025-01-06 11:10:04 -0300

    ajustando para docker

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,12 +2,27 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
+import fs from 'fs';
+import path from 'path';
+
+
 export default defineConfig({
+
+    server: {
+        https: {
+          key: fs.readFileSync(path.resolve(__dirname, 'certs/privkey.pem')),
+          cert: fs.readFileSync(path.resolve(__dirname, 'certs/cert.pem')),
+        },
+        // Make sure the server is accessible over the local network
+        host: 'agilizando.local.dev',
+    },
+
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
             refresh: true,
         }),
-        react(),
+        react(),      
+        
     ],
 });
