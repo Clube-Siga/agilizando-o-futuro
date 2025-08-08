e24a3e1d5292a00d38da008cd07abf1cfc0bc828
Webert Maximiano <webertcoach@gmail.com>
2025-08-08 16:06:53 -0300

    conflito secret app key portainer vs padrao laravel

--- src/config/app.php
+++ src/config/app.php
@@ -97,10 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    // 'key' => env('APP_KEY'),
-
-    // ✅ CORREÇÃO: Adiciona o prefixo que o Laravel espera
-    'key' => 'base64:' . env('APP_KEY'),
+    'key' => env('APP_KEY'),
     
     'previous_keys' => [
         ...array_filter(
